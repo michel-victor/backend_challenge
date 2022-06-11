4a03d12b183b098e78d64c19445bfa65973529cf
Michel Victor <mvictor86@gmail.com>
2022-06-11 20:14:16 +0200

    Refactored merchant disburse. #3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -15,12 +15,12 @@ class Merchant < ApplicationRecord
     year = year.to_i rescue nil
 
     # Generate and validate week date range
-    week_date_range = Merchant.week_date_range(date, week, year)
+    week_date_range = Merchant.week_date_range date, week, year
 
     # Get merchant orders completed between week date range and sum amounts with fee
-    amount = orders.completed.where(completed_at: week_date_range).sum{ |order| order.amount_with_fee }
+    amount = orders.completed.where(completed_at: week_date_range).sum &:disbursement_amount
 
     # Persist disbursement amount for a merchant in a week.
-    Disbursement.create!(merchant: self, amount: amount, date: week_date_range.first)
+    disturments.create! amount: amount, date: week_date_range.first
   end
 end
